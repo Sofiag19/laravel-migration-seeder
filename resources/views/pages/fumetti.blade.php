93d909c7f88581fd043b5335e6f28a5605bfb722
Sofiag19 <sofia.giacometti.19@gmail.com>
2020-01-15 19:02:47 +0100

    quasi stampa in pagina

--- resources/views/pages/fumetti.blade.php
+++ resources/views/pages/fumetti.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.app')
 
-@section('content')
-    <ol>
-        @foreach($movies as $movie)
-            <li>
-                <ul>
-                    <li>{{ $movie->title }}</li>
-                    <li>{{ $movie->year }}</li>
-                    <li>{{ $movie->overview }}</li>
-                </ul>
-            </li>
-            <br>
-        @endforeach
-    </ol>
+@section('fumetti')
+<p>ciao</p>
+<ol>
+    @foreach($fumetti as $fumetto)
+    <li>
+        <ul>
+            <li>{{ $fumetto->titolo }}</li>
+            <li>{{ $fumetto->autore }}</li>
+            <li>{{ $fumetto->anno }}</li>
+            <li>{{ $fumetto->recensione }}</li>
+            <li>{{ $fumetto->voto }}</li>
+        </ul>
+    </li>
+    <br>
+    @endforeach
+</ol>
 
 @endsection
